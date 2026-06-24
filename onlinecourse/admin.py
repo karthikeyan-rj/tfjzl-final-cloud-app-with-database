bf8067470ad58a3ea7042b2735ae3ccdcf1ef98b
Karthikeyan R J <karthikeyanrj@student.tce.edu>
2026-06-24 06:54:41 -0400

    Add exam section to course details page

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,36 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
-
-
+# Import models
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+# Inline for Lesson inside Course admin page
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
-
-# Register your models here.
+# Inline for Choice inside Question admin page
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+# Inline for Question if needed
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+# Course admin
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
-
-
+# Question admin
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
+# Lesson admin
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
-
-
-# <HINT> Register Question and Choice models here
-
+# Register models
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
